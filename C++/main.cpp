24bae4458fa587546ec4b4a9fcd20606aa518280
Alfons <xiaohuihui100@gmail.com>
2018-11-10 23:30:09 +0800

    1、新增cmake

--- C++/main.cpp
+++ C++/main.cpp
@@ -3,10 +3,16 @@
 //
 #include <iostream>
 
+#include "config.h"
 #include "DeleteRepeatElem.h"
 
 int main() {
+    fprintf(stdout, "Version -> %d.%d\n", PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR);
+
+#ifdef USE_NO_REPEAT_LIB
     DelRepeatElems();
+#endif
+
     std::cout << "helloworld" << std::endl;
     return 1;
 }
